0db948c94f0c7ffce0cb94507d4aee38aa88b053
Josefinna <josefinalafertedesign@gmail.com>
2023-09-12 17:47:38 -0300

    Versión final solemne I

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { compileFunction } from "vm";
+//import { compileFunction } from "vm";
 
-console.log("Hola personita de internet n.n <33")
+console.log("Hola persona de Internet :D")
 
 const tasks = document.querySelectorAll('.task');
 const columns = document.querySelectorAll('.column');
@@ -12,32 +12,50 @@ tasks.forEach(task => {
 
 columns.forEach(column =>{
     column.addEventListener('dragover', dragOver);
-    column.addEventListener('dragenter', dragEnter);
+    // column.addEventListener('dragenter', dragEnter);
     column.addEventListener('dragleave', dragLeave);
     column.addEventListener('drop', dragDrop);
+    column.addEventListener('click', mouseClick);
+
 });
 
 let draggedTask = null;
 
-function dragStart(){
-    draggedTask = this;
-    setTimeout(()=> (this.style.display = 'none'), o);
+function mouseClick(e){
+    const nuevatarea=document.createElement('div')
+    nuevatarea.classList.add('task')
+    nuevatarea.draggable = true
+    nuevatarea.innerHTML=prompt("Agregar nueva tarea")
+    if(e.target.appendChild(nuevatarea)){
+    }
 }
+function dragStart(e){
+    draggedTask = e.target;
+    e.dataTransfer.setData("texto", e.target.id) = "task"
+}
+
 function dragEnd(){
-    draggedTask = null;
-    setTimeout(()=> (this.style.display = ''), o);
+    // draggedTask = null;
 }
 function dragOver(e){
     e.preventDefault();
 }
 function dragEnter(e){
+    // e.preventDefault();
+    // this.style.backgroundColor = 'rgb (0, 0, 0)';
+}
+function dragLeave(e){
+    console.log(e)
+}
+
+function dragDrop(e){
+    // 
     e.preventDefault();
-    this.style.backgroundColor = 'rgb (0, 0, 0)';
+   if(e.target.classList.contains('column')){
+    e.target.appendChild(draggedTask)
+    console.log(e)
+   }
 }
-function dragleave(){
-    this.style.backgroundColor = '';
-}
-function dragDrop(){
-    this.apprendChild(draggedTask);
-    this.style.backgroundColor = '';
+function nuevatarea(e){
+    const nuevatarea =document.createElement
 }
