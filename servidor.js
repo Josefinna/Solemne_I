8cc27efe8bd529c8a337db2f497410202e80e7b3
Josefinna <josefinalafertedesign@gmail.com>
2023-09-12 17:56:45 -0300

    Versión final

--- servidor.js
+++ servidor.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = 8000
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     console.log('GET request recibida.')
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(PORT, () => {
